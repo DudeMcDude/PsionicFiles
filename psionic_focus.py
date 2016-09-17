bd91129db7a48ca592cf170f592233e75084f9c2
Dude McDude <DudeMcDude@users.noreply.github.com>
2016-09-17 10:24:26 +0300

    Update psionic_focus.py
    Hooked to Psi Depleted
    Made the Psi Focus radial appear only when Current Psi > 0

--- psionic_focus.py
+++ psionic_focus.py
@@ -4,7 +4,15 @@ import tpdp
 import char_class_utils
 import d20_action_utils
 
+
+psiMeditateEnum = 200
+psiFocusExpendConceEnum = 201
+
+
 def PsionicFocusRadial(attachee, args, evt_obj):
+	cur_psi = attachee.d20_query("Current Psi")
+	if cur_psi <= 0:
+		return 0
 	isAdded = attachee.condition_add_with_args("Psionic Focus",0,0) # adds the "Psionic Focus" condition on first radial menu build
 	isFocused = 0
 	isConcentrate = 0
@@ -81,20 +89,20 @@ def PsionicFocusLost(attachee, args, evt_obj):
 	#args.set_arg(3, 0) # unused
 	return 0
 	
+	
 psiFocus = PythonModifier("Psionic Focus", 4) 													# arg0 - is focused; arg1 - is checked for concentration; arg2 - unused; arg3 - unused
 psiFocus.AddHook(ET_OnD20PythonQuery, "Psionically Focused", PsionicallyFocused, ()) 			# hook PsionicallyFocused to event of python call
 psiFocus.AddHook(ET_OnD20PythonQuery, "Psionic Concentration", PsionicConcentration, ()) 		# hook PsionicConcentration to event of python call
 psiFocus.AddHook(ET_OnNewDay, EK_NEWDAY_REST, PsionicFocusLost, ()) 							# hook PsionicFocusLost to event of resting 8 hours safely
-#psiFocus.AddHook(?, ?, PsionicFocusLost, ()) 							   		 				 # hook PsionicFocusLost to event of becoming unconscious
-#psiFocus.AddHook(?, ?, PsionicFocusLost, ()) 													 # hook PsionicFocusLost to event of having 0 psi points
+#psiFocus.AddHook(?, ?, PsionicFocusLost, ()) 							   	# hook PsionicFocusLost to event of becoming unconscious
+psiFocus.AddHook(ET_OnD20PythonSignal, "Psi Depleted", PsionicFocusLost, ()) 				# hook PsionicFocusLost to event of having 0 psi points
 psiFocus.AddHook(ET_OnD20PythonSignal, "Expend Focus", PsionicFocusLost, ())					# hook PsionicFocusLost to event of python signal
-psiFocus.AddHook(ET_OnD20PythonActionCheck, "Meditate", OnPsionicFocusCheck, ())				# hook OnPsionicFocusCheck to event of radial menu option
-psiFocus.AddHook(ET_OnD20PythonActionPerform, "Meditate", OnPsionicFocusPerform, ())			# hook OnPsionicFocusPerform to event of radial menu option
-psiFocus.AddHook(ET_OnD20PythonActionCheck, "Concentrate Expend", OnPsionicConceCheck, ())		# hook OnPsionicConceCheck to event of radial menu option
-psiFocus.AddHook(ET_OnD20PythonActionCheck, "Concentrate Expend", OnPsionicConcePerform, ())	# hook OnPsionicConcePerform to event of radial menu option
+psiFocus.AddHook(ET_OnD20PythonActionCheck, psiMeditateEnum, OnPsionicFocusCheck, ())				# hook OnPsionicFocusCheck to event of radial menu option
+psiFocus.AddHook(ET_OnD20PythonActionPerform, psiMeditateEnum, OnPsionicFocusPerform, ())			# hook OnPsionicFocusPerform to event of radial menu option
+psiFocus.AddHook(ET_OnD20PythonActionCheck, psiFocusExpendConceEnum, OnPsionicConceCheck, ())		# hook OnPsionicConceCheck to event of radial menu option
+psiFocus.AddHook(ET_OnD20PythonActionCheck, psiFocusExpendConceEnum, OnPsionicConcePerform, ())	# hook OnPsionicConcePerform to event of radial menu option
 
-# TODO somehow make a signal appear from psi_points every time depleted is increased, so that I can hook PsionicFocusLost to the event of psi_points being depleted when depleted >= 0
+
 # TODO figure out how to add a buff to the character portrait, simply the little flag that will be named "Psionic Focus", simply for player output purpose
 # TODO make it so meditating is a full round action and provokes an attack of opportunity
 # TODO allow psionic focus toggle somehow so when you are already focused, you can make it so the next concentration check expends your focus for a free 15 roll
-# TODO fix the radial building function, it will work on EVERY pc in the game, internal to function only makes it work if PC has current psi points and concentration points
