1f9cccab87a3a4d1236dad203205de8902579ca4
Psionics-ToEE <Arthur.Patten@live.com>
2016-09-22 15:06:56 -0600

    Fixed modifier to add from feat. I claim 41 as the Insight Bonus.

--- feat_speed_of_thought.py
+++ feat_speed_of_thought.py
@@ -5,17 +5,17 @@ import char_class_utils
 import d20_action_utils
 
 def SpeedOfThought(attachee, args, evt_obj):
-  isFocused = attachee.d20_query("Psionically Focused") # Character must be psionically focused
+  isFocused = attachee.d20_query("Psionically Focused")             # Character must be psionically focused
   if not isFocused:
     return 0
   
-  armor = obj.item_worn_at(5)                           # Character must not be wearing heavy armor
+  armor = obj.item_worn_at(5)                                       # Character must not be wearing heavy armor
     if armor != OBJ_HANDLE_NULL:
         armorFlags = armor.obj_get_int(obj_f_armor_flags)
         if armorFlags == ARMOR_TYPE_HEAVY:
             return 0
-          
-  evt_obj.bonus_list.add(10, ?, 114)                    # Add 10 insight bonus to move speed
+            
+  evt_obj.bonus_list.add_from_feat(10, 41, 114, "Speed of Thought") # Add 10 insight bonus to move speed
   return 0
 
 feat_speed_of_thought = PythonModifier("feat_speed_of_thought", 1)
