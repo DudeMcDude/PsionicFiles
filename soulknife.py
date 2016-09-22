12298312d199a00b5ac18160876cc611820dcc74
Psionics-ToEE <Arthur.Patten@live.com>
2016-09-22 01:08:15 -0600

    Create soulknife.py class modifier file
    Are any other enums needed at the top? Basic stuff here right now, will later add modifiers for all of the 11 class specific feats that .txt files were created for. No need to use maptofeat I believe, those are only so that feat list will display it ingame, the class feats will be handled by checking attachee.stat_level_get(classEnum).

---
+++ soulknife.py
@@ -0,0 +1,40 @@
+from templeplus.pymod import PythonModifier
+from toee import *
+import tpdp
+import char_class_utils
+import d20_action_utils
+###################################################
+
+def GetConditionName():
+	return "Soulknife"
+
+print "Registering " + GetConditionName()
+
+classEnum = Soulknife
+
+###################################################
+
+
+#### standard callbacks - BAB and Save values
+def OnGetToHitBonusBase(attachee, args, evt_obj):
+	classLvl = attachee.stat_level_get(classEnum)
+	babvalue = game.get_bab_for_class(classEnum, classLvl)
+	evt_obj.bonus_list.add(babvalue, 0, 137) # untyped, description: "Class"
+	return 0
+
+def OnGetSaveThrowFort(attachee, args, evt_obj):
+	value = char_class_utils.SavingThrowLevel(classEnum, attachee, D20_Save_Fortitude)
+	evt_obj.bonus_list.add(value, 0, 137)
+	return 0
+
+def OnGetSaveThrowReflex(attachee, args, evt_obj):
+	value = char_class_utils.SavingThrowLevel(classEnum, attachee, D20_Save_Reflex)
+	evt_obj.bonus_list.add(value, 0, 137)
+	return 0
+
+def OnGetSaveThrowWill(attachee, args, evt_obj):
+	value = char_class_utils.SavingThrowLevel(classEnum, attachee, D20_Save_Will)
+	evt_obj.bonus_list.add(value, 0, 137)
+	return 0
+
+#### class modifiers
