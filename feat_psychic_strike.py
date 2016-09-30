352aba437a977f19471a98b8e859909908333a7d
Dude McDude <DudeMcDude@users.noreply.github.com>
2016-09-30 16:02:59 +0300

    Update feat_psychic_strike.py
    Psy Strike corrections

--- feat_psychic_strike.py
+++ feat_psychic_strike.py
@@ -4,6 +4,8 @@ import tpdp
 import char_class_utils
 import d20_action_utils
 
+psiStrikeEnum = 202 # enum for Psy Strike action
+
 def PsychicStrikeRadial(attachee, args, evt_obj):
   isEmbued = attachee.d20_query("PsyStrike Embued")
   if isEmbued:
@@ -12,10 +14,29 @@ def PsychicStrikeRadial(attachee, args, evt_obj):
   radialAction.add_child_to_standard(attachee, tpdp.RadialMenuStandardNode.Class)
   return 0
   
-def PsychicStrikeDamage(attachee):
-  slkLvl = attachee.stat_level_get(Soulknife)
+def PsychicStrikeDamage(attachee, args, evt_obj):
+  if not IsEmbued(args):
+    return 0
+  # Check if target is living, nonmindless, and not immune to mind affecting effects
+  tgt = evt_obj.attack_packet.target
+  if tgt == OBJ_HANDLE_NULL:
+    return 0
+  if tgt.is_category_type(mc_type_undead):
+    return 0
+  # todo immune to mind affecting / mindless
+  
+  slkLvl = attachee.stat_level_get(stat_level_soulknife)
   bonval = (slkLvl+1)/4 # Psychic Strike bonus damage is bonval d8's
-  return bonval
+  if bonval <= 0:
+    return 0
+  
+  dice = dice_new('1d8')
+  dice.number = bonval
+  evt_obj.damage_packet.add_dice(dice, D20DT_UNSPECIFIED, 100) # will add xd8 unspecified damage type, and list the cause as "Weapon" (entry 100 in damage.mes)
+  args.set_arg(0, 0)  # Set to unembued
+  return 0
+  
+  
   
 def PsyStrikeEmbued(attachee, args, evt_obj):
   evt_obj.return_val = IsEmbued(args)
@@ -32,28 +53,19 @@ def OnPsyStrikeCheck(attachee, args, evt_obj):
 def OnPsyStrikePerform(attachee, args, evt_obj):
 	args.set_arg(0, 1)  # Set to embued
   return 0 
-
-def PsyStrikeHit(attachee, args, evt_obj):
-  isEmbued = attachee.d20_query("PsyStrike Enbued")
-  if isEnbued:
-    return 0
-  # Check if weapon hitting is a mind blade type
-  # If not, return 0
-  # Check if target is living, nonmindless, and not immune to mind affecting effects
-  # If not, return 0
-  # add to bonus value = PsychicStrikeDamage d8's
-  args.set_arg(0, 0)  # Set to unembued
+  
   
 def PsyStrikeEffectTooltip(attachee, args, evt_obj):
-  isEmbued = attachee.d20_query("PsyStrike Enbued")
+  isEmbued = attachee.d20_query("PsyStrike Embued")
   if not isEmbued:
     return 0
-  evt_obj.append(?, ?, ?) # 53 is the indicator graphical index; buffs (indicators above portraits) are in the 0-89 range IIRC; will have to expand this to support new icons
-return 0
+  evt_obj.append(53, -2, "Psy Strike Eenrgy") # 53 is the indicator graphical index; buffs (indicators above portraits) are in the 0-89 range IIRC; will have to expand this to support new icons
+  return 0
 
 psychicStrike = PythonModifier("Psychic Strike", 1)                                                 # arg0 - embued with energy
-psiFocus.AddHook(ET_OnD20PythonQuery, "PsyStrike Embued", PsyStrikeEmbued, ())                      # hook PsyStrikeEmbued to event of python query
+psychicStrike.AddHook(ET_OnD20PythonQuery, "PsyStrike Embued", PsyStrikeEmbued, ())                 # hook PsyStrikeEmbued to event of python query
 psychicStrike.AddHook(ET_OnBuildRadialMenuEntry, EK_NONE, PsychicStrikeRadial, ())                  # Add to radial menu
-psychicStrike.AddHook(ET_OnD20PythonActionCheck, "Psychic Strike Radial", OnPsyStrikeCheck, ())     # Check if valid
-psychicStrike.AddHook(ET_OnD20PythonActionPerform, "Psychic Strike Radial", OnPsyStrikePerform, ()) # Perform if valid
+psychicStrike.AddHook(ET_OnD20PythonActionCheck, psiStrikeEnum, OnPsyStrikeCheck, ())     # Check if valid
+psychicStrike.AddHook(ET_OnD20PythonActionPerform, psiStrikeEnum, OnPsyStrikePerform, ()) # Perform if valid
+psychicStrike.AddHook(ET_OnDealingDamage, EK_NONE, PsychicStrikeDamage, ())                         # Damage callback, adds xd8 damage dice
 psychicStrike.MapToFeat("Psychic Strike")                                                           # Map to feat
