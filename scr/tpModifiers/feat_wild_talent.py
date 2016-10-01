8d381fac83e4186f257d89cc0be930b747bb3fa3
Dude McDude <DudeMcDude@users.noreply.github.com>
2016-10-01 09:00:11 +0300

    Update feat_wild_talent.py (#17)
    * Update feat_wild_talent.py
    
    Made some corrections
    
    Should Wild Talent affect the Base Max Psi too? For purposes of determining feat eligibility and such.
    
    Couple of notes:
    You do not use Constructors for PythonModifiers! The only thing a PythonModifier's constructor will do is register it in the grand list of modifiers, that's it.
    
    You can add a hook for ET_OnConditionAdd which will go off whenever a modifier is isntantiated onto a specific object. But even there you should be very careful!
    
    * Rename feat_wild_talent.py to scr/tpModifiers/feat_wild_talent.py
    
    * Rename scr/tpModifiers/feat_wild_talent.py to feat_wild_talent.py
    
    * Changed to affect Base Max Psi and renamed

---
+++ scr/tpModifiers/feat_wild_talent.py
@@ -0,0 +1,14 @@
+from templeplus.pymod import PythonModifier
+from toee import *
+import tpdp
+import char_class_utils
+import d20_action_utils
+
+def WildTalentMaxPsi(attachee, args, evt_obj):
+  #attachee.d20_send_signal("Increase Max Psi", 2)
+  evt_obj.return_val += 2
+  return 0
+
+feat_wild_talent = PythonModifier("feat_wild_talent", 1)
+feat_wild_talent.MapToFeat("Wild Talent")
+feat_wild_talent.AddHook(ET_OnD20PythonQuery, "Base Max Psi", WildTalentMaxPsi, ())
