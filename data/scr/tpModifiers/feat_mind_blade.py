5bb74becea23fd70d577a9d0f066ac6cc35602b8
Dude McDude <DudeMcDude@users.noreply.github.com>
2016-10-21 19:06:54 +0300

    Update feat_mind_blade.py (#20)
    * Update feat_mind_blade.py
    
    * Update feat_mind_blade.py

--- data/scr/tpModifiers/feat_mind_blade.py
+++ data/scr/tpModifiers/feat_mind_blade.py
@@ -36,8 +36,28 @@ def OnMindBladePerform(attachee, args, evt_obj):
 	# Create Mind Blade object in primary hand of attachee
 	return 0
 
-mindBlade = PythonModifier("Mind Blade", 0)
-mindBlade.AddHook(ET_OnBuildRadialMenuEntry, EK_NONE, MindBladeRadial, ())                  # Add to radial menu
-mindBlade.AddHook(ET_OnD20PythonActionCheck, mindBladeEnum, OnMindBladeCheck, ())     # Check if valid
-mindBlade.AddHook(ET_OnD20PythonActionPerform, mindBladeEnum, OnMindBladePerform, ()) # Perform if valid
-mindBlade.MapToFeat("Mind Blade")                                                           # Map to feat
+# Mind Blade feat Modifier
+mindBladeFeat = PythonModifier("Feat Mind Blade", 0)
+mindBladeFeat.AddHook(ET_OnBuildRadialMenuEntry, EK_NONE, MindBladeRadial, ())                  # Add to radial menu
+mindBladeFeat.AddHook(ET_OnD20PythonActionCheck, mindBladeEnum, OnMindBladeCheck, ())     # Check if valid
+mindBladeFeat.AddHook(ET_OnD20PythonActionPerform, mindBladeEnum, OnMindBladePerform, ()) # Perform if valid
+mindBladeFeat.MapToFeat("Mind Blade")                                                           # Map to feat
+
+
+# this is the modifier to be applied to the blade itself in protos.tab
+def MindBladeMagicDamage(attachee, args, evt_obj):
+	evt_obj.damage.attack_power |= D20DAP_MAGIC
+	return 0
+
+def MindBladeWeaponGlow(attachee, args, evt_obj):
+	evt_obj.damage.attack_power |= D20DAP_MAGIC
+	inven_idx = args.get_arg(2)
+	item = attachee.inventory_item(inven_idx)
+	if item != OBJ_HANDLE_NULL and (item == attachee.item_worn_at(3) or item == attachee.item_worn_at(4)):
+		evt_obj.return_val = 3 # originally used in "Weapon Bane", feel free to experiment with this
+	return 0
+
+
+mindBlade = PythonModifier("Mind Blade", 5) # args(2) is the inventory index (set by the engine), the rest reserved
+mindBlade.AddHook(ET_OnDealingDamage2, EK_NONE, MindBladeMagicDamage, ())
+mindBlade.AddHook(ET_OnWeaponGlowType, EK_NONE, MindBladeWeaponGlow, ())
